977139cadc86f31bee9dca46c3ed103922f3a6b1
Text_r <CoreText@users.noreply.github.com>
2022-07-17 15:58:34 +0300

    check monad interface

--- AkelFiles/Plugs/Scripts/Include/ES/my_polyfills.js
+++ AkelFiles/Plugs/Scripts/Include/ES/my_polyfills.js
@@ -1,12 +1,14 @@
-/**
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
- */
-Array.prototype.flatMap = function (callbackFn, thisArg) {
-  return this.reduce(function (acc, x) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return acc.concat(callbackFn.apply((thisArg || callbackFn), args));
-  }, []);
-};
+if (!Array.prototype.flatMap) {
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
+   */
+  Array.prototype.flatMap = function (callbackFn, thisArg) {
+    return this.reduce(function (acc, x) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      return acc.concat(callbackFn.apply((thisArg || callbackFn), args));
+    }, []);
+  };
+}
 
 function isNaN(value) {
   return value !== value;
